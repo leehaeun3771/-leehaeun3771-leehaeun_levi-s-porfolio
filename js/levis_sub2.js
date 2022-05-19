012589a3a04ff39788433237322017d5273e1f2c
leehaeun3771 <leehaeun3771@gmail.com>
2022-05-19 17:55:07 +0900

    20220519 18:00

--- js/levis_sub2.js
+++ js/levis_sub2.js
@@ -14,6 +14,19 @@ $(function(){
     });
 
 
+    cntTxt = $('.content.cnt02 section article');
+
+    cntTxt.click(function(){
+
+        cntTxt.find('.image').removeClass('on1');
+        cntTxt.find('.texts').removeClass('on2');
+
+        $(this).find('.image').addClass('on1');
+        $(this).find('.texts').addClass('on2');
+
+    }); 
+
+
     // 탑버튼
     $('.Topbtns .top').click(function(){
         $('html,body').stop().animate({scrollTop : 0},500);
